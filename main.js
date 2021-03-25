99b485279224f12c13182734549ebf18467a1109
Bakhtiyor <bakhtiyor.542@mail.ru>
2021-03-25 15:03:51 +0500

    try to fix

--- main.js
+++ main.js
@@ -1,42 +1,44 @@
-function fetch(url){
-    $('#loader').show();
-    $.get('https://my-api.plantnet.org/v2/identify/all',{
-        'api-key': '2b10HqJuexpyAGFncT0jXVvPzO',
-        'images': url,
-        'organs':'leaf'
-    })
-    .done(function(res){
-        render(res.results[0].species);
+$(function(){
+    function fetch(url){
+        $('#loader').show();
+        $.get('https://www.my-api.plantnet.org/v2/identify/all',{
+            'api-key': '2b10HqJuexpyAGFncT0jXVvPzO',
+            'images': url,
+            'organs':'leaf'
+        })
+        .done(function(res){
+            render(res.results[0].species);
+            $('#loader').hide();
+            $('#alert').hide();
+            
+        })
+        .fail(function(error){
+            $('#result').hide();
+            $('#loader').hide();
+            $('#alert').text(error.responseJSON.error+': '+error.responseJSON.message);
+            $('#alert').show();
+        });
+    }
+    
+    function render(data){
+        $('#img').attr('src',$('#url').val());
+        $('#names').text(data.commonNames);
+        $('#sname').text(data.scientificNameWithoutAuthor);
+        $('#genus').text(data.genus.scientificNameWithoutAuthor);
+        $('#family').text(data.family.scientificNameWithoutAuthor);
+        $('#result').show();
+    
+    }
+    
+    function find(event){
+        event.preventDefault();
+        url=$('#url').val();
+        if(url!='')
+            fetch(url);
+    }   
+    function wait(){
         $('#loader').hide();
         $('#alert').hide();
-        
-    })
-    .fail(function(error){
         $('#result').hide();
-        $('#loader').hide();
-        $('#alert').text(error.responseJSON.error+': '+error.responseJSON.message);
-        $('#alert').show();
-    });
-}
-
-function render(data){
-    $('#img').attr('src',$('#url').val());
-    $('#names').text(data.commonNames);
-    $('#sname').text(data.scientificNameWithoutAuthor);
-    $('#genus').text(data.genus.scientificNameWithoutAuthor);
-    $('#family').text(data.family.scientificNameWithoutAuthor);
-    $('#result').show();
-
-}
-
-function find(event){
-    event.preventDefault();
-    url=$('#url').val();
-    if(url!='')
-        fetch(url);
-}   
-function wait(){
-    $('#loader').hide();
-    $('#alert').hide();
-    $('#result').hide();
-}
+    }
+})
